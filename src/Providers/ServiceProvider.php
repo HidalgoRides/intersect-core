8ff893ac6d98164e3e5b2e66d4febf4c7f413109
Tim Kipp <timkipp.22.developer@gmail.com>
2019-08-12 22:33:23 -0700

    Added core service provider class

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Intersect\Core\Providers;
+
+use Intersect\Core\Container;
+
+abstract class ServiceProvider {
+
+    /** @var Container */
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function init() {}
+
+}
